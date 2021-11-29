68dddba2a115afccfa35dc15053a130066b38332
Brecht-CodeLab <brecht.vandekerkhove@student.kuleuven.be>
2021-11-29 23:56:17 +0100

    changes

--- Code/robospace.py
+++ Code/robospace.py
@@ -1,10 +1,17 @@
 from classes.Angle import Angle
 from classes.Connection import Connection
+import RPI.GPIO as GPIO
 import numpy as np
+import time
 
+# PARAMETERS
 L = np.array([1,1])
-angle = np.array([np.pi/2, 0.56, 1.58])
+angle = np.array([np.pi/4, 0.56, 1.58])
 pos = np.array([0,0,1])
+
+# SETUP
+GPIO.setmode(GPIO.BOARD)
+
 
 next_angles = Angle(angle, pos, L)
 
