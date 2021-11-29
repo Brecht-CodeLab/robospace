f7c7f3e9176a2ff75a95524fc86817d4308de1ec
Brecht-CodeLab <brecht.vandekerkhove@student.kuleuven.be>
2021-11-29 14:42:45 +0100

    changes

--- Code/classes/Connection.py
+++ Code/classes/Connection.py
@@ -1,18 +1,25 @@
-from paramiko import SSHClient
+# from paramiko import SSHClient
 import os
 
 
 class Connection:
 
-    def __init__(self):
-        self.client = SSHClient()
+    def __init__(self, ):
+        # self.client = SSHClient()
         self.variables = os.environ
 
 
-        self.client.connect('', username='', password='')
+        # self.client.connect(self.variables['IP'], self.variables['USER'], self.variables['PASSW'])
 
-        return self.client
+        # return self.client
 
 
     def closeConnection(self):
-        self.client.close()
+        # self.client.close()
+        pass
+
+
+
+
+
+    
